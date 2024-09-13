5b4691904901747bbf1d86144a54fe2b8645d908
Hamza <6793siddique@gmail.com>
2024-09-13 23:38:33 +0500

    Added different logic fort games slider

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,4 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    /**
+     * Swiper for first section "Games"
+     */
+    // Select all Swiper containers with the same class
+    const swiperContainersGame = document.querySelectorAll('.games_swiper');
+    console.log('page loaded');
+    
+    if (swiperContainersGame.length) {
+        // Function to set `slides-per-view` attribute based on window width
+            const windowWidth = window.innerWidth;
+            let slides = 3;
+
+            if (windowWidth < 500) { // Mobile
+                slides = 1;
+            } else if (windowWidth < 1000) { // Tablet
+                slides = 2;
+            }
+
+            // Apply changes to all swiper containers
+            swiperContainersGame.forEach(function(swiperContainer) {
+                swiperContainer.setAttribute('slides-per-view', slides);
+                console.log(windowWidth);
+            console.log(slides);
+            });
+    }
+
+
     // Select all Swiper containers with the same class
     const swiperContainers = document.querySelectorAll('.mySwiper');
     console.log('page loaded');
